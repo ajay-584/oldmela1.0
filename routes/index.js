994795069c636c70809fe50647923eb82775d007
Ajay Kumar <sliet584@gmail.com>
2020-11-03 22:13:23 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'oldmela.com' });
 });
+
+
+
+// ========================================= sell ads sections ==================================================
+/* GET sell ads pate */
+router.get('/sell_ads', function(req, res, next){
+  res.render('users/sell_ads', { title: 'oldmela.com' });
+});
+
+
 
 module.exports = router;
