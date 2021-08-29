746e73abdc59148d3f6db8a145cbba1b3871e817
Ajay Kumar <ajay.kumar@antiersolutions.com>
2021-08-29 14:31:11 +0530

    just add for digit and etc

--- helper/otpGenerator.js
+++ helper/otpGenerator.js
@@ -3,7 +3,7 @@ const randomstring = require("randomstring");
 // Generating 6 digit random no for otp
 const otpGenerator = () => {
   const a = randomstring.generate({
-  length: 6,
+  length: 4,
   charset: 'numeric'
   });
   // console.log("Your otp is ",parseInt(a));
