8648fc7eb088fe4b08592150e9e0f4a8d491d218
Ajay Kumar <ajay.kumar@antiersolutions.com>
2021-09-06 20:08:09 +0530

    store ip

--- helper/visitorCounter.js
+++ helper/visitorCounter.js
@@ -4,18 +4,19 @@ const pool = require('../model/pool');
 module.exports = () => {
 
 	return async (req, res, next) => {
-		// custom code 
-		const host = req.headers.host;
-		const ip = req.ip;
-		// determine the today date
-		const todayDate = dateFormat(new Date(), 'dd-mm-yyyy');
-		// await pool.visitor_data.create({visitData:"data"});
-		const hostIpDate = String(`hostp:${host}|ip:${ip}|date:${todayDate}`);
-		const visit = await pool.visitor_data.find({visitData:hostIpDate});
-		if(visit.length<1){
-			await pool.visitor_data.create({visitData:hostIpDate});
-			return next();
+		try{
+			// custom code 
+			const date = dateFormat(new Date(), 'dd-mm-yyyy');
+			const ip = req.ip;
+			const visit = await pool.visitor_data.find({$and:[{ip},{date}]});
+			if(visit.length<1){
+				await pool.visitor_data.create({ip,date});
+				return next();
+			}
+			next();
+		}catch(e){
+			console.log(e);
+			next();
 		}
-		next();
 	};
 };
--- model/pool.js
+++ model/pool.js
@@ -64,8 +64,8 @@ var city_model = mongoose.model('city_model', city_schema,'city_data');
 
 // express visitor counter
 const visitor_schema = mongoose.Schema({
-    visitData:String,
-    date:{typeDate, default:Date.now()}
+    ip:String,
+    date:String
 });
 var visitor_model = mongoose.model('visitor_mode', visitor_schema, 'visitor_data');
 
