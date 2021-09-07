ea5e2c7b10989c4b2af4ac6e2169f978685a67ea
Ajay Kumar <ajay.kumar@antiersolutions.com>
2021-09-07 20:45:10 +0530

    just

--- controller/contactController.js
+++ controller/contactController.js
@@ -4,7 +4,7 @@ const pool = require('./../model/pool');
 exports.contactGet = async(req, res, next)=>{
     try{ 
         let session = req.session;
-        return res.render('contactUs', {user_name: session.name, fail:'', pass:''});
+        return res.render('userContact', {user_name: session.name, fail:'', pass:''});
     }catch(e){
         console.log(e);
         next();
@@ -18,7 +18,7 @@ exports.contactPost = async(req, res, next)=>{
         const phone_or_email = req.body.email_or_phone;
         const message = req.body.message;
         await pool.contact_data.create({name, phone_or_email, message});
-        return res.render('contactUs', {user_name: session.name, fail:'', pass:`Dear ${name} message has been sent. Thank you!`});
+        return res.render('userContact', {user_name: session.name, fail:'', pass:`Dear ${name} message has been sent. Thank you!`});
     }catch(e){
         console.log(e);
         next();
